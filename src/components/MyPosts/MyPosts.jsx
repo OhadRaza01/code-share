35be138e9d1af5647f3e00697345ab2aea94da74
OhadRaza01 <ohadraza345@gmail.com>
2026-05-29 02:42:18 +0500

    onSnapshot listener s added in MyPost.jsx

--- src/components/MyPosts/MyPosts.jsx
+++ src/components/MyPosts/MyPosts.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { getDocs, collection, query, where } from 'firebase/firestore'
+import { getDocs, collection, query, where, onSnapshot } from 'firebase/firestore'
 import { db } from '../../firebase'
 import { useAuth } from '../Context/AuthContext'
 import Post from '../Feed/Post'
@@ -12,21 +12,23 @@ export default function MyPosts() {
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        async function fetchPosts() {
-            const q = query(
-                collection(db, "posts"),
-                where("userId", "==", user.uid) 
-            )
-            const snapshot = await getDocs(q)
+        const q = query(
+            collection(db, "posts"),
+            where("userId", "==", user.uid)
+        );
+
+        const unsub = onSnapshot(q, (snapshot) => {
             const postsArray = snapshot.docs.map((doc) => ({
                 id: doc.id,
                 ...doc.data(),
-            }))
-            setPosts(postsArray)
-            setLoading(false)
-        }
-        fetchPosts()
-    }, [])
+            }));
+
+            setPosts(postsArray);
+            setLoading(false);
+        });
+
+        return () => unsub(); // cleanup
+    }, []);
 
     return (
         <main className="flex-1 max-w-3xl px-4">
@@ -36,7 +38,9 @@ export default function MyPosts() {
                     <div className="text-gray-400 text-center py-10">Loading posts...</div>
                 ) : (
                     posts.map((post) => (
-                        <Post key={post.id} char={post.username.charAt(0)} name={post.username} time={post.createdAt?.toDate().toLocaleDateString()} language={post.codeLanguage} title={post.title}
+                        <Post upvotedBy={post.upvotedBy}
+                            downvotedBy={post.downvotedBy}
+                            postId={post.id} key={post.id} char={post.username.charAt(0)} name={post.username} time={post.createdAt?.toDate().toLocaleDateString()} language={post.codeLanguage} title={post.title}
                             code={post.code} upvotes={post.upvotes} downvotes={post.downvotes} comments={post.comments} />
                     ))
                 )}
